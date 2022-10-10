df119b63e41496e95f24ec337bf53aa8aab584ad
NeDonil <gatfild@bk.ru>
2022-10-10 22:21:35 +0300

    Add vacation duration in date format

--- src/main/java/com/NeDonil/VacationCalc/controller/VacationCalculatorController.java
+++ src/main/java/com/NeDonil/VacationCalc/controller/VacationCalculatorController.java
@@ -1,11 +1,16 @@
 package com.NeDonil.VacationCalc.controller;
 
 import com.NeDonil.VacationCalc.service.VacationCalculatorService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+
+@RequestMapping("/vacation_calc")
 @Controller
 public class VacationCalculatorController {
 
@@ -15,12 +20,26 @@ public class VacationCalculatorController {
         this.service = service;
     }
 
-    @GetMapping("/vacation_calc")
-    ResponseEntity calcSalary(@RequestParam(name = "mid_salary", defaultValue = "0.0") double midSalary,
-                              @RequestParam(name = "vacation_days", defaultValue = "0") int vacationDays){
+    @GetMapping("/days")
+    ResponseEntity calcSalaryByDayCount(@RequestParam(name = "mid_salary", defaultValue = "0.0") double midSalary,
+                              @RequestParam(name = "vacation_days", defaultValue = "0") long vacationDays){
         try {
             return ResponseEntity.ok()
                     .body(service.calcSalary(midSalary, vacationDays));
+        } catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/dates")
+    ResponseEntity calcSalaryByDates(@RequestParam(name = "mid_salary", defaultValue = "0.0") double midSalary,
+                              @RequestParam(name = "vacation_begin")
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vacationBegin,
+                              @RequestParam(name = "vacation_end")
+                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate vacationEnd){
+        try {
+            return ResponseEntity.ok()
+                    .body(service.calcSalary(midSalary, vacationBegin, vacationEnd));
         } catch(Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/com/NeDonil/VacationCalc/service/VacationCalculatorService.java
+++ src/main/java/com/NeDonil/VacationCalc/service/VacationCalculatorService.java
@@ -3,12 +3,29 @@ package com.NeDonil.VacationCalc.service;
 import com.NeDonil.VacationCalc.exception.CalculatorOperandsException;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 @Service
 public class VacationCalculatorService {
-    public double calcSalary(double midSalary, int vacationDays) throws CalculatorOperandsException {
+    public double calcSalary(double midSalary, long vacationDays) throws CalculatorOperandsException {
         if(midSalary < 0 || vacationDays < 0){
             throw new CalculatorOperandsException("Operands must be positive numbers");
         }
+
+        double vacationInMonths = vacationDays / 30.0;
+        return vacationInMonths * midSalary; // vacation salary
+    }
+
+    public double calcSalary(double midSalary,
+                             LocalDate vacationBegin,
+                             LocalDate vacationEnd) throws CalculatorOperandsException {
+        long vacationDays = Duration.between(vacationBegin.atStartOfDay(), vacationEnd.atStartOfDay()).toDays();
+
+        if(midSalary < 0 || vacationDays < 0){
+            throw new CalculatorOperandsException("The end of the vacation must be later than the start");
+        }
+
         double vacationInMonths = vacationDays / 30.0;
         return vacationInMonths * midSalary; // vacation salary
     }
